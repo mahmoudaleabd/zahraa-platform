d8e245d4de771d65b7f40dca0be3e87de937f4b7
mahmoudaleabd <mahmoud.aleabd@gmail.com>
2026-06-23 15:39:27 +0300

    page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,4 +1,6 @@
-import LoginContent from './LoginContent';
+import dynamic from 'next/dynamic';
+
+const LoginContent = dynamic(() => import('./LoginContent'), { ssr: false });
 
 export default function LoginPage() {
   return <LoginContent />;
